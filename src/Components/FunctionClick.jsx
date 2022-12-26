201256579daa8241327ef47a5080f3f9145a9b43
Archana Upadhyay <archua1229@gmail.com>
2022-12-26 21:06:38 +0530

    added message in react project

---
+++ src/Components/FunctionClick.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FunctionClick = () => {
+    function clickHandler(){
+        
+    }
+  return (
+    <div>
+      <button onClick={URL="https://vitejs.dev/guide/api-javascript.html"}>Click Me</button>
+       
+    </div>
+  );
+}
+
+export default FunctionClick;
